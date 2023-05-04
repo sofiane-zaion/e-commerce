a1eb94ef2f72aa71e55f382b21313cf844504880
sofiane-zaion <123477481+sofiane-zaion@users.noreply.github.com>
2023-05-04 19:39:55 +0200

    Delete include directory

--- include/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn = 'mysql:host=db5012548593.hosting-data.io;dbname=dbs10547750';
-$username = 'dbu2099475';
-$password = 'Nador92230';
-
-$pdo = new PDO($dsn, $username, $password);
-
-?>
